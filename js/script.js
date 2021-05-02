2cf84b1fd5affa11009083524769bcbe87676de3
Ahmed-Elkassas <ahmed.elkassas2811@gmail.com>
2021-05-02 11:39:46 +0200

    upload the portfolio design

---
+++ js/script.js
@@ -0,0 +1,61 @@
+
+const skillSection = document.getElementById('skills');
+const skillsBar = document.querySelectorAll('.info');
+
+window.addEventListener('scroll', () => {
+    const sectionPos = skillSection.getBoundingClientRect().top;
+    const screenPos = window.innerHeight;
+
+    if(sectionPos < sectionPos) {
+        console.log(`show progress`);
+    } else {
+        console.log('hide progress');
+    }
+})
+
+
+$(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20) {
+            $('.nav-bar').addClass('sticky');
+        } else {
+            $('.nav-bar').removeClass('sticky');
+        }
+        if (this.scrollY > 500) {
+            $('.scroll-up-btn').addClass('show');
+        } else {
+            $('.scroll-up-btn').removeClass('show');
+        }
+    });
+
+    // Toggle the nav menu
+    $('.nav-bar .menu-bar').click(function(){
+
+        $('.nav-bar .menu').toggleClass('active');
+        $('.nav-bar .menu-bar i').toggleClass('active');
+
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    // typed animation script 
+    const typed1 = new Typed(".typing", {
+        strings: ["Developer", "Designer", "Freelancer", "Programmer"],
+        typeSpeed: 80,
+        backSpeed: 60,
+        loop: true
+    });
+
+    const typed2 = new Typed(".typing-2", {
+        strings: ["Programmer", "Developer", "Designer", "Designer"],
+        typeSpeed: 80,
+        backSpeed: 60,
+        loop: true
+    });
+})
+
